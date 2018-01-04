03ffed6b134a98c0cbc19624e852767baf2f9878
Daniel <dhidalgosanchez96@gmail.com>
2018-01-04 21:49:24 +0100

    Add delimiter in testSR.py. New csv.

--- testSR.py
+++ testSR.py
@@ -3,10 +3,11 @@ import sys
 import contentBased as cB
 sys.path.append('./Scripts')
 
-data = pd.read_csv("recipes.csv", error_bad_lines=False)
+data = pd.read_csv("recipes.csv", error_bad_lines=False, delimiter=";")
 data.head(3)
 
 content_based = cB.ContentBased()
+
 content_based.fit(data, 'recipe_str')
 
-print(content_based.predict(['Mayonesa patata']))
+print(content_based.predict(['cebolla pasta']).get('cook_url').values)
